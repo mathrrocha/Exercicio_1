193c6de65c926b333b009e6953fc93cdf3008380
mathrrocha <48018570+mathrrocha@users.noreply.github.com>
2019-03-26 09:35:43 -0300

    Update MainPage.xaml.cs

--- Exercicio_1/MainPage.xaml.cs
+++ Exercicio_1/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +13,34 @@ namespace Exercicio_1
 		{
 			InitializeComponent();
 		}
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            // desabilitar mensagens
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            // verificar se período e semestre foram selecionados
+            if(Entry2.Text != null && Entry2.Text.Lenght > 0 && Picker.SelectedIndex >= 0)
+            {
+                // criar variáveis
+                int periodo = int.Parse(Entry2.Text);
+                int semestre = Picker.SelectedIndex + 1;
+                // verificar se os valores são iguais
+                if(periodo == semestre)
+                {
+                    // habilitar mensagem de sucesso
+                    Label1 = IsVisible = true;
+                }else
+                {
+                    // habilitar mensagem de erro
+                    Label2 = IsVisible = true;
+                }
+            }
+            else
+            {
+                // habilitar mensagem de erro
+                Label2.IsVisible = true;
+            }
+        }
 	}
 }
